0d89869f75ae4f616167e9df24fb8321df5cb7e4
NIRMITHA <avishkanirmitha@gmail.com>
2021-09-25 12:43:56 +0530

    set up enviramental variable and finished natores api section now time to learn mongo db

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const morgan =require('morgan');
 const tourRouter = require('./routes/tourRouts');
 const userRouter = require('./routes/userRoutes');
 
+
 const app=express();
 app.use(express.json());
  
+// console.log(process.env.PORT);
 
 // Middleweres
-app.use(morgan('dev'));
+if(process.env.NODE_ENV==='development'){
+    console.log(`development`);
+    app.use(morgan('dev'));
+}
 // app.use((req,res,next)=>{
 //     console.log(`hello to the middlewere ✋`);
 //     next();
@@ -25,5 +30,6 @@ app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours',tourRouter);
 app.use('/api/v1/users',userRouter);
+
 
 module.exports =app;
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+const dotenv = require("dotenv");
+dotenv.config({path:'./config.env'});
+
 const app = require("./app");
 
+
 //Server
-const port=8000;
+const port=process.env.PORT || 8000;
 app.listen(port,()=>{
     console.log('App running on port ');
 });
